0626218c3a1d64bc71983185e6ae904099a8b71d
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2022-09-15 15:44:38 +0530

    feat: extracted faces saving in detected-faces

--- face-rec-v1/face_extract.py
+++ face-rec-v1/face_extract.py
@@ -3,12 +3,15 @@ from retinaface import RetinaFace
 import matplotlib.pyplot as plt
 import cv2
 import pandas as pd
+import os
 
 img_1_path = r"face-rec-v1\testing-dataset\aum.jpeg"
 img_2_path = r"face-rec-v1\testing-dataset\sb.jpeg"
 img_3_path = r"face.jpeg"
 
-faces = RetinaFace.extract_faces(img_path = img_1_path, align = True)
+path = r"face-rec-v1\detected-faces"
+
+faces = RetinaFace.extract_faces(img_path = img_1_path, align = False)
 
 model_name = 'VGG-Face'
 
@@ -20,15 +23,17 @@ face_num=1
 
 for face in faces:
     rgb_face=cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
-    cv2.imwrite(f"face{face_num}.jpeg",rgb_face)
+    
+    cv2.imwrite(os.path.join(path , f"face{face_num}.jpeg"), rgb_face)
+    # cv2.imwrite(f"face-rec-v1\testing-dataset\face{face_num}.jpeg",rgb_face)
     cv2.imshow('Face',rgb_face)
     cv2.waitKey(2000)
     face_num+=1
 
 # for index in range(1,face_num):
-img_path = r"face1.jpeg"
-response = DeepFace.verify(img1_path=img_path, img2_path=img_2_path, model_name=model_name)
-print(response)
+# img_path = r"face1.jpeg"
+# response = DeepFace.verify(img1_path=img_path, img2_path=img_2_path, model_name=model_name)
+# print(response)
 #     response2 = DeepFace.verify(img1_path=img_1_path, img2_path=img_2_path, model_name=model_name, detector_backend='retinaface')
 #     print(response2)
 # response2 = DeepFace.verify(img1_path=img_1_path, img2_path=img_2_path, model_name=model_name, detector_backend='retinaface')
